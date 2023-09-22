e4f7c4f6a3ba48a6fb3f9a80094f892dc1f9bb4c
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-22 16:11:39 +0530

    checked different data types

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Variables
+﻿using System;
+
+namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            byte number = 23;
+            float totalPrice = 23.22f;
+            bool isWorking = true;
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine(number);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(isWorking);
         }
     }
 }
