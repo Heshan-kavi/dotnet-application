26c9d4ce36ff11ad6d793e702b232ace22f63459
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-30 18:49:27 +0530

    removed unneccessary else blocks in the code

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -384,30 +384,28 @@ namespace Variables
             Console.WriteLine(returnedSummarizedString);
         }
 
-        static string SummerizingText(string sentence, int maxLength)
+        static string SummerizingText(string sentence, int maxLength = 20)
         {
             if (sentence.Length < maxLength)
             {
                 return sentence;
             }
-            else
+
+            var words = sentence.Split(' ');
+            var totalCharacters = 0;
+            var summaryTextList = new List<string>();
+
+            foreach (var word in words)
             {
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryTextList = new List<string>();
-
-                foreach (var word in words)
+                totalCharacters += word.Length + 1;
+                summaryTextList.Add(word);
+                if (totalCharacters > maxLength)
                 {
-                    totalCharacters += word.Length + 1;
-                    summaryTextList.Add(word);
-                    if (totalCharacters > maxLength)
-                    {
-                        break;
-                    }
+                    break;
                 }
-
-                return string.Join(' ', summaryTextList) + " ...";
             }
+
+            return string.Join(' ', summaryTextList) + " ...";
         }
     }
 }
