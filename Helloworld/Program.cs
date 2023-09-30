ce4b863fd85a5ded1988dc23b7f2eaa56c761dba
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-01 01:38:00 +0530

    moved type related functionalities into a new class called “TypesClass”

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -147,15 +147,7 @@ namespace Variables
 
 
             //types
-            //var numberOne = 12;
-            //var numberTwo = numberOne;
-            //numberTwo++;
-            //Console.WriteLine("This is the number one : {0} and this is number two : {1}", numberOne, numberTwo);           //value type
-
-            //var arrayOne = new int[3]{ 1, 2, 3};
-            //var arrayTwo = arrayOne;
-            //arrayTwo[0] = 55;
-            //Console.WriteLine("This is the number one : {0} and this is number two : {1}", arrayOne[0], arrayTwo[0]);       //reference type
+            Helloworld.TypesClass.TypeRelatedFunctionalities();
 
 
             //conditional statements
---
+++ Helloworld/TypesClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Helloworld
+{
+	public class TypesClass
+	{
+		public static void TypeRelatedFunctionalities()
+		{
+            var numberOne = 12;
+            var numberTwo = numberOne;
+            numberTwo++;
+            Console.WriteLine("This is the number one : {0} and this is number two : {1}", numberOne, numberTwo);           //value type
+
+            var arrayOne = new int[3] { 1, 2, 3 };
+            var arrayTwo = arrayOne;
+            arrayTwo[0] = 55;
+            Console.WriteLine("This is the number one : {0} and this is number two : {1}", arrayOne[0], arrayTwo[0]);       //reference type
+        }
+    }
+}
+
