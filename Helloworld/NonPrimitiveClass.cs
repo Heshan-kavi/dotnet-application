01da725a9b34f22fe76a192a4af1543a89606e93
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-01 09:35:30 +0530

    moved relevant functionalities into different classes according to the type

---
+++ Helloworld/NonPrimitiveClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Variables;
+using Helloworld.Math;
+namespace Helloworld
+{
+	public class NonPrimitiveClass
+	{
+		public static void NonPrmitiveTypeRelatedFunctionalities()
+		{
+            Person person = new Person();
+            person.FirstName = "Heshan";
+            person.Secondname = "Kavinda";
+            person.age = 23;
+            person.address = "Meegoda";
+            var fullName = person.Introduce();
+            Console.WriteLine("this is the full name : " + fullName);
+
+            //testing a class which is located in a different field
+            Calculator calculator = new Calculator();
+        Console.WriteLine("This is the resulf from the cal : " + calculator.AddNumbers(12, 23));
+        }
+    }
+}
+
--- Helloworld/TypesClass.cs
+++ Helloworld/TypesClass.cs
@@ -15,6 +15,29 @@ namespace Helloworld
             arrayTwo[0] = 55;
             Console.WriteLine("This is the number one : {0} and this is number two : {1}", arrayOne[0], arrayTwo[0]);       //reference type
         }
+
+        public static void BasicTypeConversions()
+        {
+            //explicit conversion
+            int testingvalue = 260;
+            byte testingValueTwo = (byte)testingvalue;
+            Console.WriteLine("this is the value in int type :  {0}", testingvalue);
+            Console.WriteLine("this is the value in byte type :  {0}", testingValueTwo);
+
+            //implicit conversion
+            int testingValueThree = 260;
+            float testingValueFour = testingValueThree;
+            Console.WriteLine("this is the value in int type :  {0}", testingValueThree);
+            Console.WriteLine("this is the value in float type :  {0}", testingValueFour);
+
+            //non-compatible conversion
+            string testingValueFive = "12";
+            float testingValueSix = Convert.ToInt16(testingValueFive);
+            float testingValueSeven = int.Parse(testingValueFive);
+            Console.WriteLine("this is the value in string type :  {0}", testingValueSix);
+            Console.WriteLine("this is the value in int type with convert function :  {0}", testingValueSix);
+            Console.WriteLine("this is the value in int type with parse function :  {0}", testingValueSeven);
+        }
     }
 }
 
