c7dbdb80f74e68d1995718e2e59f014fdc34733b
Hesh-Kavinda <heshan@ascnetic.se>
2023-10-02 15:09:13 +0530

    used some testing tools in the c# and the visual studio code

---
+++ Helloworld/Debugging.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Helloworld
+{
+	public class Debugging
+	{
+		public Debugging()
+		{
+			//for debugging we can use debug mode (fn + f5);
+			//add breaking points by going through debug view or the shortcut.
+			//to go into a bug point we can use (fn + 11)
+			//step over (fn + f11)
+			//we can delete all the breakpoints by using debug->delete/disable all breakpoints
+			//we can use the call stack window which comes under the debug window to identify the flow of execution of the functions
+			//through auto window we can get the automatic list of variaables that we are interested this is done by automatically.
+			//through local window we can get the list of variables in the current local scope.
+		}
+	}
+}
+
